1ea659d19719c0e03f5375e2f3745608ee3c445a
hamzayogurtcuoglu <hamzayogurtcuoglu@gmail.com>
2022-01-30 15:37:28 +0300

    Adding a new transaction to the Blockchain

--- epochcoin.py
+++ epochcoin.py
@@ -137,7 +137,23 @@ def is_valid():
         response = {'message': 'The Blockchain is not valid.'}
     return jsonify(response), 200
 
+
+# Adding a new transaction to the Blockchain
+@app.route('/add_transaction', methods=['POST'])
+def add_transaction():
+    json = request.get_json()
+    transaction_keys = ['sender', 'receiver', 'amount']
+    if not all(key in json for key in transaction_keys)
+        return 'Some elements of the transaction are missing', 400
+    index = blockchain.add_transaction(json['sender'], json['receiver'], json['amount'])
+    response = {
+        'message': f'This transaction will be added to Block {index}'}
+    return jsonify(response), 201
 # Decentralizing blockchain
+
+# Connecting new nodes
+
+
 
 # Running App
 app.run(host = '0.0.0.0', port = 5000)
