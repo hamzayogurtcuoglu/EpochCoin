3cd5332646c65ea3d06fbcc3983fba6411b38a1a
hamzayogurtcuoglu <hamzayogurtcuoglu@gmail.com>
2022-01-30 15:59:53 +0300

    Connect new nodes.

--- epochcoin.py
+++ epochcoin.py
@@ -152,8 +152,17 @@ def add_transaction():
 # Decentralizing blockchain
 
 # Connecting new nodes
-
-
-
+@app.route('/connect_node', methods=['POST'])
+def connect_node()
+    json = request.get_json()
+    nodes = json.get('nodes')
+    if nodes is None:
+        return "No node", 400
+    for node in nodes:
+        blockchain.add_node(node)
+    response = {'message': 'All the nodes are now connected. The Epoch coin Blockchain now contains the following nodes:',
+                'total_nodes': list(blockchain.nodes)}
+    return jsonify(response), 201
+    
 # Running App
 app.run(host = '0.0.0.0', port = 5000)
