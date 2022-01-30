a65a7a52c24ccd5024391f9e1223840582c13c77
hamzayogurtcuoglu <hamzayogurtcuoglu@gmail.com>
2022-01-30 16:30:54 +0300

    Preparation demo for Epoch Coin.

--- epochcoin_node_5003.py
+++ epochcoin_node_5003.py
@@ -109,7 +109,7 @@ def mine_block():
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
-    blockchain.add_transaction(sender = node_address, receiver = 'Hamza', amount = 1)
+    blockchain.add_transaction(sender = node_address, receiver = 'Veli', amount = 1)
     block = blockchain.create_block(proof, previous_hash)
     response = {'message': 'Congratulations, you just mined a block!',
                 'index': block['index'],
@@ -177,4 +177,4 @@ def replace_chain():
     return jsonify(response), 200
 
 # Running App
-app.run(host = '0.0.0.0', port = 5000)
+app.run(host = '0.0.0.0', port = 5003)
